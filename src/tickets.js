9a5c87baf73ee83c6dee06f74d8c84f3aac69060
nurulamin092 <mnurulamin092@gmial.com>
2022-07-29 22:58:36 +0600

    updateById

--- src/tickets.js
+++ src/tickets.js
@@ -37,13 +37,43 @@ class TicketCollection {
      */
 
 
-    findTicketById(id) {
+    findById(id) {
         const ticket = this[tickets].find(
             /**
              * @param {Ticket}ticket
              */
             (ticket) => ticket.id == id
         );
+        return ticket
+    }
+
+    /**
+     * find tickets by username
+     * @param {string} username 
+     * @return {Ticket[]}
+     */
+    findByUsername(username) {
+        const tickets = this[tickets].filter(
+            /**
+             * @param {Ticket} ticket
+             */
+            (ticket) => ticket.username == username
+        );
+        return tickets
+    }
+
+    /**
+     * update by id
+     * @param {string} ticketId 
+     * @param {{username:string,price:number}} ticketBody 
+     * @return {Ticket}
+     */
+
+    updateById(ticketId, ticketBody) {
+        const ticket = this.findById(ticketId)
+        ticket.username = ticketBody.username ?? ticket.username;
+        ticket.price = ticketBody.price ?? ticket.price;
+
         return ticket
     }
 }
