ce68d7bb382b2f0b1202f18867ee1a72afa13ee3
nurulamin092 <mnurulamin092@gmial.com>
2022-07-29 21:50:49 +0600

    first commit

---
+++ src/Ticket.js
@@ -0,0 +1,21 @@
+const shortid = require('shortid');
+
+
+class Ticket {
+    /**
+     * Ticket constructor will receive username and price
+     * @param {string} username 
+     * @param {number} price 
+     */
+    constructor(username, price) {
+        this.id = shortid.generate();
+        this.username = username;
+        this.price = price;
+        this.createAt = new Date();
+        this.updateAt = new Date();
+
+
+    }
+}
+
+module.exports = Ticket; 
